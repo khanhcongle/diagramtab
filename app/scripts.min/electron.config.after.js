8ceb88aa2ddb488c48e5da89511a7132acf47e75
lckhanh <lecongkhanh382@gmail.com>
2019-10-19 21:09:43 +0700

    Khanh - fix error log in console during load chrome extension

---
+++ app/scripts.min/electron.config.after.js
@@ -0,0 +1,2 @@
+//Fix error "jQuery not found" Insert this line after script imports
+if (window.module) module = window.module;
---
+++ app/scripts.min/electron.config.before.js
@@ -0,0 +1,2 @@
+// Fix error "jQuery not found" Insert this line above script imports 
+if (typeof module === 'object') {window.module = module; module = undefined;}
